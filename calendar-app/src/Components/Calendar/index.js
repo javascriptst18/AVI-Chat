226d951776c0f4984d58d7b7fabfb74977ab5caf
Alan_H <alanhabib.atb@gmail.com>
2018-09-09 15:24:36 +0200

    change

---
+++ calendar-app/src/Components/Calendar/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import moment from "moment";
+
+class Calendar extends React.Component {
+  render() {
+    return (
+      <div className="calendar-container">
+        <h2>Calendar</h2>
+      </div>
+    );
+  }
+}
+
+export default Calendar;
